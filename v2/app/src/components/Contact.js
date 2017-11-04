aa652fe882f50d3773f1fa3e78f455cc2ae9d3f1
James McGill <contact@jamesmcgill.co.uk>
2017-11-04 08:25:35 +0000

    Added comment

--- v2/app/src/components/Contact.js
+++ v2/app/src/components/Contact.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Send from 'react-icons/lib/md/send'
 
+
+
 class ContactForm extends Component {
   constructor(props) {
     super(props)
@@ -18,15 +20,23 @@ class ContactForm extends Component {
       [name]: event.target.value
     })
   }
+  // var name = document.querySelector('#name')
+  // name.addEventListener('input', (event) => {
+  //   if (name.vaildity.typeMismatch) {
+  //     name.setCustomValidity('Please enter your name')
+  //   } else {
+  //     name.setCustomValidity('')
+  //   }
+  // })
   render() {
     return (
       <form action='https://formspree.io/contact@jamesmcgill.co.uk' method='POST'>
         Name:
-        <input type='text' name='name' value={this.state.name} onChange={this.handleChange}/>
+        <input type='text' name='name' id='name' value={this.state.name} onChange={this.handleChange} required/>
         E-mail:
-        <input type='email' name='email' value={this.state.email} onChange={this.handleChange}/>
+        <input type='email' name='email' id='email' value={this.state.email} onChange={this.handleChange} required/>
         Message:
-        <textarea name='message' value={this.state.message} onChange={this.handleChange}>
+        <textarea name='message' id='message' value={this.state.message} onChange={this.handleChange} required>
         </textarea>
         <div className='submitBtn'>
           <button className='button' type='submit'>Submit</button>
