b7a787f58b6897820b3a0d654ba9abdfa7a8fab0
James McGill <contact@jamesmcgill.co.uk>
2017-11-04 18:13:05 +0000

    Added form validation, link to Github account and mailto: e-mail link

--- v2/app/src/components/Contact.js
+++ v2/app/src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import Send from 'react-icons/lib/md/send'
-
+import Github from 'react-icons/lib/fa/github'
+import Envelope from 'react-icons/lib/fa/envelope'
 
 
 class ContactForm extends Component {
@@ -20,28 +20,59 @@ class ContactForm extends Component {
       [name]: event.target.value
     })
   }
-  // var name = document.querySelector('#name')
-  // name.addEventListener('input', (event) => {
-  //   if (name.vaildity.typeMismatch) {
-  //     name.setCustomValidity('Please enter your name')
-  //   } else {
-  //     name.setCustomValidity('')
-  //   }
-  // })
+  componentDidMount() {
+    let form = document.querySelector('#form')
+    let name = document.querySelector('#name')
+    let email = document.querySelector('#email')
+    let message = document.querySelector('#message')
+    let nameError = document.querySelector('#name-error')
+    let emailError = document.querySelector('#email-error')
+    let messageError = document.querySelector('#message-error')
+
+    form.addEventListener('submit', (event) => {
+      if (name.value.length == 0) {
+        name.classList.add('error-input')
+        nameError.classList.add('error')
+        nameError.innerText = 'Please enter your name'
+        event.preventDefault()
+      }
+      if (email.value.length == 0) {
+        email.classList.add('error-input')
+        emailError.classList.add('error')
+        emailError.innerText = 'Please enter a vaild email address'
+        event.preventDefault()
+      }
+      if (message.value.length == 0) {
+        message.classList.add('error-input')
+        messageError.classList.add('error')
+        messageError.innerText = "Don't forget to say something!"
+        event.preventDefault()
+      }
+    })
+  }
   render() {
     return (
-      <form action='https://formspree.io/contact@jamesmcgill.co.uk' method='POST'>
-        Name:
-        <input type='text' name='name' id='name' value={this.state.name} onChange={this.handleChange} required/>
-        E-mail:
-        <input type='email' name='email' id='email' value={this.state.email} onChange={this.handleChange} required/>
-        Message:
-        <textarea name='message' id='message' value={this.state.message} onChange={this.handleChange} required>
-        </textarea>
-        <div className='submitBtn'>
-          <button className='button' type='submit'>Submit</button>
+      <div id='contact-container'>
+        <form id='form' action='https:formspree.io/contact@jamesmcgill.co.uk' method='POST'>
+          Name:
+          <input type='text' name='name' id='name' value={this.state.name} onChange={this.handleChange} />
+          <span id='name-error'></span>
+          E-mail:
+          <input type='email' name='email' id='email' value={this.state.email} onChange={this.handleChange} />
+          <span id='email-error'></span>
+          Message:
+          <textarea name='message' id='message' value={this.state.message} onChange={this.handleChange}>
+          </textarea>
+          <span id='message-error'></span>
+          <div className='submitBtn'>
+            <button className='button' type='submit'>Submit</button>
+          </div>
+        </form>
+        <div id='contact-icons'>
+          <a href='https://github.com/jimbomags' target='_blank'><Github /></a>
+          <a href='mailto:contact@jamesmcgill.co.uk'><Envelope /></a>
         </div>
-      </form>
+      </div>
     )
   }
 }
