4f8b5231ae0259736e216df72a86bd8a46fa561f
James McGill <contact@jamesmcgill.co.uk>
2018-08-30 14:52:06 +0100

    Changed Bookmark App CSS to make it responsive

--- App/src/App.js
+++ App/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 
 import Home from './components/Home';
